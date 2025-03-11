1d5a737e254ff6aac37a356e00619fd042517ede
PES2UG22CS678 <PES2UG22CS678@pesu.pes.edu>
2025-03-11 19:51:24 +0530

    hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES2UG22CS678\n";
-  cout << "Yogendra A\n";
+  cout << "Yogendra\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
